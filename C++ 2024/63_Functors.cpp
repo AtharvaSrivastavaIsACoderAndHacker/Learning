78801606585fadd606a7e7173817e7f1b770fc3e
Atharva Srivastava <atharva201004@gmail.com>
2025-03-09 23:44:44 +0530

    9/3/2025 23:44 ++1CppFile(63)

---
+++ C++ 2024/63_Functors.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <functional>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    
+    // Functors are functions in a class that can be called through objects of the class by operator overloading !
+    // STL includes some pre-built functors for our use; just like templates, functors are also pre-built into STL for our use as a utility !
+    
+    // Later, I'll  explain this in another file, it takes time to digest STL thingy !
+
+    return 0;
+}
